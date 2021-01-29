1d53bfa2da63dcfdac571067c1b641464c8ab668
Bill Njoroge <williamsriunge@gmail.com>
2021-01-29 13:59:03 +0300

    changd test

--- pyspotify/tests/test_authentication.py
+++ pyspotify/tests/test_authentication.py
@@ -6,6 +6,7 @@ from pyspotify.core import read_config_file
 config = read_config_file()
 class TestAuthentication:     
      def test_read_config_file():
+          '''Test ability to read config file'''
           config = read_config_file()
           assert  isinstance(config, dict)
      def test_contains_client_id():
